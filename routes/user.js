959123944bf8852483e0ff7d2316513ab7c5da3d
LorensHuculak <huculaklorens@yahoo.com>
2017-08-26 16:12:33 +0200

    Init Passport.JS

--- routes/user.js
+++ routes/user.js
@@ -2,13 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/login', function (req, res, next) {
+router.get('/login', function (req, res) {
 
     res.render('login');
 
 });
 
-router.get('/register', function (req, res, next) {
+router.get('/register', function (req, res) {
     res.render('register');
     
 });
--- server.js
+++ server.js
@@ -4,6 +4,18 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require ('body-parser');
 var hbs = require('express-handlebars');
+
+var cookieParser = require('cookie-parser');
+
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var Primus = require('primus');
+
+// init server
 var app = express();
 
 //POSTING
@@ -11,15 +23,61 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(cookieParser());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.')
+            , root    = namespace.shift()
+            , formParam = root;
+
+        while(namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param : formParam,
+            msg   : msg,
+            value : value
+        };
+    }
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
+    next();
+});
 
 
 
-var Primus = require('primus');
+
+
 var mongoose = require('mongoose');
 var Discussion = require('./models/Discussion.model.js');
 var Question = require('./models/Question.model.js');
 var Answer = require('./models/Answer.model.js');
-var mongo = require('mongodb').MongoClient;
+var mongow = require('mongodb').MongoClient;
 var client = require('socket.io').listen(3005).sockets;
 
 
@@ -38,7 +96,7 @@ Discussion.find({})
 
 // Connect to MongoDB
 var counter = 0;
-mongo.connect('mongodb://127.0.0.1/qanda', function(err, db) {
+mongow.connect('mongodb://127.0.0.1/qanda', function(err, db) {
    if(err){
       throw err;
    }
